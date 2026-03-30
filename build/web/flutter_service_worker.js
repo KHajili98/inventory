90039678d672a13de444a6a2193b70f4466d21f2
Kamran Hacili <khajili98@gmail.com>
2026-03-30 04:03:28 +0400

    Build: Update web build for deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "0661ef4243f73210fc472a12135575aa",
+const RESOURCES = {"flutter_bootstrap.js": "6b6f0c1b12d842d8411fc6ced54d21c7",
 "version.json": "6ff04479ae5bf44c167e1a7fdbf94ec2",
 "index.html": "ccd810d4eae508957c135904616194b8",
 "/": "ccd810d4eae508957c135904616194b8",
-"main.dart.js": "201e8dcd4f1ad2b1044abacdfe2a127d",
+"main.dart.js": "38e7bf903d7ff28357aad5a63084cd6d",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
