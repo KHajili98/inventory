22af6d1672529c6cbc130982406fe40becb2f1d9
Kamran Hacili <khajili98@gmail.com>
2026-04-07 23:44:07 +0400

    Build: Update web build for deployment

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3989870422"
+    serviceWorkerVersion: "2814080820"
   }
 });
